9f21ced5f9d53c4680c7ac283a760cd3ae7bb15d
jonahvimahi <jonahvimahi@gmail.com>
2021-08-19 16:23:11 -0600

    added more functionality to cipher

--- cypher.js
+++ cypher.js
@@ -1,17 +1,35 @@
-function cipher(str) {
-const normal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+function unCipher(str) {
+const normal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
 
-const key = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' ]
+const key = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ' ']
 
 const resultStr = [];
 for (let i = 0; i < str.length; i++){
-    for(let j = 0; j < normal.length; j++){
-        if(str[i] === normal[j]){
-            resultStr.push(key[j])
+    for(let j = 0; j < key.length; j++){
+        if(str[i] === key[j]){
+            resultStr.push(normal[j])
         }
     }
   }
   return resultStr.join("")
 };
 
-console.log(cipher('qhhst'))
+function cipher(str) {
+    const normal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+    
+    const key = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ' ']
+    
+    const resultStr = [];
+    for (let i = 0; i < str.length; i++){
+        for(let j = 0; j < normal.length; j++){
+            if(str[i] === normal[j]){
+                resultStr.push(key[j])
+            }
+        }
+      }
+      return resultStr.join("")
+    };
+
+console.log(unCipher('qhhst ol eggs'))
+
+console.log(cipher('what is up'))
